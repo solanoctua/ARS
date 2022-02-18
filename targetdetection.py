c5c5fd68ebe257dad08b9f504d1635c5163ef76b
berberfurkan <furkanberber.resmi@hotmail.com>
2022-02-19 02:08:07 +0300

    fpsTrackerAdded

--- targetdetection.py
+++ targetdetection.py
@@ -1,5 +1,6 @@
-import cv2
+import cv2 , time
 import numpy as np
+
 
 def nothing(x):
     pass
@@ -12,13 +13,19 @@ cv2.createTrackbar("max - H", "Trackbars", 6, 179, nothing)
 cv2.createTrackbar("max - S", "Trackbars", 255, 255, nothing)
 cv2.createTrackbar("max - V", "Trackbars", 255, 255, nothing)
 cam = cv2.VideoCapture(0)
+prev_frame_time = 0
+new_frame_time = 0
 if cam.isOpened():
     ret,frame = cam.read()
 else: 
     ret = False
 while ret :
+    new_frame_time = time.time()
+    fps = 1/(new_frame_time-prev_frame_time)
+    prev_frame_time = new_frame_time
     ret,frame = cam.read()
-    frame = cv2.resize(frame,(480,480))
+    frame_width, frame_height = (480,480)
+    frame = cv2.resize(frame,(frame_width, frame_height))
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     H,S,V = cv2.split(hsv_frame)
     #Red Color
@@ -37,26 +44,28 @@ while ret :
     
     mask_red = cv2.inRange(hsv_frame, min_red, max_red)
     red = cv2.bitwise_and(frame, frame, mask = mask_red)
+    cv2.putText(red,"FPS:{}".format(int(fps)),(15,15),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255),1,cv2.LINE_AA)#Displays fps
+    cv2.circle(red, (frame_width//2, frame_height//2), 25, (255,0,0), 1)
     contours, hierarchy = cv2.findContours(mask_red , cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #print("The number of red objects in the frame is {}".format(len(contours)-1))
     
     if len(contours) >= 1:
         # Sort all the contours wrt their areas to discard the small objects
         red_objects = sorted(contours, key=cv2.contourArea)
-        red_objects = red_objects[-1:] # Take the 3 objects with the largest area
+        red_objects = red_objects[-2:] # Take the 3 objects with the largest area
         for object in red_objects: 
             #print("Area = ",cv2.contourArea(object))
             if cv2.contourArea(object) >= 500: # If area is big enough, find its center etc.
-                cv2.putText(red, "Red Object Detected", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 255), 2)
+                cv2.putText(red, "Red Object Detected", (35, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 255), 2)
                 # Get the moments https://docs.opencv.org/3.4/d8/d23/classcv_1_1Moments.html
                 moment = cv2.moments(object) # To find the center of the contour, we use cv2.moment
                 center = (moment['m10'] / (moment['m00'] + 1e-5), moment['m01'] / (moment['m00'] + 1e-5)) # calculate center of the contour
                 #print(center)
                 color = (255, 255, 255)
-                cv2.circle(red, (int(center[0]), int(center[1])), 1, color, -1) # draw circle at the center of the contour
+                cv2.circle(red, (int(center[0]), int(center[1])), 15, color, -1) # draw circle at the center of the contour
                 cv2.putText(red, "({},{})".format(int(center[0]) , int(center[1])), (int(center[0]) , int(center[1]) + 45), cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2) # write pixel coordinates
         
-        result = cv2.drawContours(red, object, -1, (0,255,0), 3) 
+        result = cv2.drawContours(red, object, -1, (0,255,0), 3,lineType=cv2.FILLED) 
         cv2.imshow("result",result)
     
     """
